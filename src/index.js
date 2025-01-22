869113bcaf283f1d495408c8f7f3b1d2dacc00aa
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-22 06:32:58 +0200

    Add event handler for '#formSearch' form that prevents default behavior

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "./styles.css";
 import { setupEventHandlers } from "./uiManager";
-import { getImage } from "./image";
 
 setupEventHandlers();
--- src/uiManager.js
+++ src/uiManager.js
@@ -4,15 +4,29 @@ import { getImage } from "./image";
 function btnSearchEventHandler() {
   let btnSearch = document.querySelector("#btnSearch");
   let textLocation = document.querySelector("#textLocation");
+  let weatherContainer = document.querySelector(".weather-container");
   btnSearch.addEventListener("click", async () => {
     try {
       let data = await getRequiredWeatherData(textLocation.value);
+      weatherContainer.replaceChildren();
       for (let i = 0; i < data.length; i++) {
         createWeatherElement(data[i]);
       }
     } catch (error) {
       console.log(error);
     }
+  });
+}
+
+/*
+  If use presses enter after typing in 'textLocation' input field, 
+  the page will reload because of the default behavior of the 
+  form element, so we need to stop that
+*/
+function formSearchEventHandler() {
+  let formSearch = document.querySelector("#formSearch");
+  formSearch.addEventListener("submit", (event) => {
+    event.preventDefault();
   });
 }
 
@@ -26,12 +40,11 @@ function createWeatherElement(data) {
   let temperature = document.createElement("p");
   let conditions = document.createElement("p");
 
-  weatherContainer.replaceChildren();
   weatherContainer.appendChild(weatherElement);
   weatherElement.classList.add("weather");
   weatherElement.appendChild(weatherElementImage);
   weatherElementImage.classList.add("weather-image");
-  getImage(`weather: ${data.icon}`).then(result =>{
+  getImage(`weather: ${data.icon}`).then((result) => {
     console.log(result.data.images.original.url);
     weatherElementImage.src = result.data.images.original.url;
   });
@@ -50,6 +63,7 @@ function createWeatherElement(data) {
 
 function setupEventHandlers() {
   btnSearchEventHandler();
+  formSearchEventHandler();
 }
 
 export { setupEventHandlers };
